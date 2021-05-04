014490afa638d748c969130834ea393dc2669a92
Vinicius Neves <vinicius.mkt2016@gmail.com>
2021-05-04 18:51:51 -0300

    Teste de Comit

---
+++ Pessoa.py
@@ -0,0 +1,7 @@
+class Pessoa():
+    def __init__(self):
+        self.nome = "Caboco"
+        self.ano_nascimento = 1999
+        self.sexo = "Masculino"
+        self.telefone = "62 9 9474-4589"
+
