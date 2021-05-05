4949dc13c8d627ec7142926e56780b848df83409
Vinicius Neves <vinicius.mkt2016@gmail.com>
2021-05-04 22:22:48 -0300

    Novos Arquivos Criados (Motorista e Veiculo)

---
+++ Main.py
@@ -0,0 +1,2 @@
+from Motorista import Motorista
+
--- Motorista.py
+++ Motorista.py
@@ -4,3 +4,6 @@ import Pessoa
 class Motorista:
     def __init__(self, chn):
         self.cnh = "a"
+
+    def verificar_cnh(self, chn):
+        if
