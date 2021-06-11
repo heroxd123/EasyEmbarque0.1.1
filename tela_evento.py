6529bfb1723129058bf7eb0ed7351cf64c9207ba
paulo <paulolrp@outlook.com>
2021-06-11 13:52:44 -0300

    Quebra de linha na tela de evento

--- tela_evento.py
+++ tela_evento.py
@@ -1,26 +1,27 @@
 import PySimpleGUI as sg
 from evento import Evento
 
-#Tela
+
 class Tela_Evento:
     def __init__(self):
+        sg.theme('Light Brown 1')
         layout = [
             [sg.Text('Nome do Evento:', size=(15, 0)), sg.Input(size=(20, 0), key='nome')],
-            [sg.Text('Codigo do Evento:', size=(15, 0)), sg.Input(size=(10, 0), key='codigo')],
-            [sg.Text('Preço do ingresso:', size=(15, 0)), sg.Input(size=(15, 0), key='ingresso')],
+            [sg.Text('Codigo do Evento:', size=(15, 0)), sg.Input(size=(10, 0), key='id')],
             [sg.Text('Data do evento:', size=(15, 0)), sg.CalendarButton('Selecionar', format='%d/%m/%Y', size=(20, 0)
             , target='data', key='calendario'),
              sg.Input('DD/MM/AAAA', size=(15, 0), key='data')],
             [sg.Text('Lote:', size=(15, 0)), sg.Combo(['Promocional', 'Primeiro', 'Segundo'], key='lote')],
+            [ sg.Text('Preço do Ingresso:', size=(15, 0)), sg.Input(size=(5, 0), key='ingresso')],
             [sg.Button('Enviar')]
         ]
 
         janela = sg.Window('Dados do Evento').layout(layout)
         self.button, self.values = janela.Read()
-#Metodo de Cadastro
+
     def cadastrar_evento(self):
         nome = self.values["nome_evento"]
-        codigo = self.values["codigo"]
+        codigo = self.values["id"]
         ingresso = self.values["ingresso"]
         data = self.values["data_evento"]
         lote = self.values["lote"]
